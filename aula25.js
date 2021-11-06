15eff02b812adc8b25686f758f4cfe1afe267d10
vivianeeee <vivianeoliveirandrade@gmail.com>
2021-11-06 03:28:04 -0300

    aula 25

---
+++ aula25.js
@@ -0,0 +1,49 @@
+let consoler = 1 ;
+
+while (contador <= 10) {
+    consoler.log('valor = ${contador}') ;
+    //contador++; // contadoer = contador + 1
+    contador = contador + 2;
+}
+
+//WHILE ESTRUTURA DE REPETIÇÃO
+ let contador2 = 10;
+
+ while (contador2 = 0){
+  console.log('valor = $ {contador2}' );
+ contador2 = contador2 -1
+}
+
+console.clear();
+// DO WHILE ESTRUTURA DE REPETIÇÃO
+let i = 0 ;
+text='';
+do {
+    text += 'O numero é ${i}/n';   
+    i++;
+} while (i <10);
+ console.log(text);
+
+ console.clear();
+ // FOR LAÇO DE REPETIÇÃO
+ for(let i = 0; i < 10; i = i + 1) {
+     if (i == 0) 
+    
+    console.log('laço for numero: ${i}') ;
+}
+
+console.clear();
+
+// LAÇOS DE REPETICÃO COM ARRAYS
+const frutas= ['Maça', 'Laranja','melancia' , 10, true];
+
+console.log(frutas);
+for(let j = 0; j < 'frutas.length');
+console.log ('o nome da fruta é ${frutas}[j]');
+
+
+
+
+
+
+
